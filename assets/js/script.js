1979f6f43e36e25af28ab10e22a5edfb3429af26
Calterat <Calterat@gmail.com>
2020-10-20 01:56:52 -0500

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,16 @@ const createTaskEl = (taskDataObj) => {
         let taskActionsEl = createTaskActions(taskIdCounter);
         listItemsEl.appendChild(taskActionsEl);
         // add entire list item to list
-        tasksToDoEl.appendChild(listItemsEl);
+        if (taskDataObj.status === 'to do') {
+            listItemsEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemsEl);
+        } else if (taskDataObj.status === 'in progress') {
+            listItemsEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemsEl);
+        } else if (taskDataObj.status === 'completed') {
+            listItemsEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemsEl);
+        }
         // add ID to task object
         taskDataObj.id = taskIdCounter;
         // push task object to tasks
@@ -242,54 +251,16 @@ const saveTasks = () => {
 }
 
 const loadTasks = () => {
+    let savedTasks = localStorage.getItem("tasks");
 
-    // New Refactored code doesn't work
-
-    // let savedTasks = localStorage.getItem("tasks");
-
-    // if (!savedTasks) {
-    //     return false;
-    // }
-    // savedTasks = JSON.parse(savedTasks);
-
-    // for (a = 0; a < savedTasks.length; a++) {
-    //     createTaskEl(savedTasks[a]);
-    // }
-
-
-    tasks = localStorage.getItem("tasks");
-
-    if (!tasks) {
-        tasks = [];
+    if (!savedTasks) {
         return false;
     }
+    savedTasks = JSON.parse(savedTasks);
 
-    tasks = JSON.parse(tasks);
-
-    for (a =0; a < tasks.length; a++) {
-        taskIdCounter = tasks[a].id;
-        let listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", tasks[a].id);
-        listItemEl.setAttribute("draggable", true);
-        let taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[a].name + "</h3><span class='task-type'>" + tasks[a].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-        let taskActionsEl = createTaskActions(tasks[a].id);
-        listItemEl.appendChild(taskActionsEl);
-        if (tasks[a].status === 'to do') {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        } else if (tasks[a].status === 'in progress') {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        } else if (tasks[a].status === 'completed') {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
+    for (a = 0; a < savedTasks.length; a++) {
+        createTaskEl(savedTasks[a]);
     }
-    taskIdCounter++;
 }
 
 // listens for a submit on the form element
